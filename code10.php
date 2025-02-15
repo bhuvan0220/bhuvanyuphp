161051aecc2028d8b349d68e76cb54d946ef88b0
bhuvan0220 <163634043+bhuvan0220@users.noreply.github.com>
2025-02-16 01:43:22 +0530

    code10.php

--- code10.php
+++ code10.php
@@ -3,13 +3,13 @@
         $x = 10;
         $y = 4;
     
-        echo "$x + $y = ". ($x + $y) . "\n"; // 0utputs: 14
-        echo "$x - $y = ". ($x - $y) . "\n"; // 0utputs: 6
-        echo "$x * $y = ". ($x * $y) . "\n"; // 0utputs: 40
-        echo "$x / $y = ". ($x / $y) . "\n"; // 0utputs: 2.5
-        echo "$x % $y = ". ($x % $y) . "\n"; // 0utputs: 2
+        echo "$x + $y = ". ($x + $y) . "<br>"; // 0utputs: 14
+        echo "$x - $y = ". ($x - $y) . "<br>"; // 0utputs: 6
+        echo "$x * $y = ". ($x * $y) . "<br>"; // 0utputs: 40
+        echo "$x / $y = ". ($x / $y) . "<br>"; // 0utputs: 2.5
+        echo "$x % $y = ". ($x % $y) . "<br><br>"; // 0utputs: 2
 	
-	echo "Program By Bhuvanyu Chhibber (2220100266)";
+    echo "Made By Ayush Kumar 086";
 	
 
 ?>
