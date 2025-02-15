1372ad87a8e10b0f463c56e9311449be65883386
bhuvan0220 <163634043+bhuvan0220@users.noreply.github.com>
2025-02-16 01:52:12 +0530

    code10.php

--- code10.php
+++ code10.php
@@ -9,7 +9,7 @@
         echo "$x / $y = ". ($x / $y) . "<br>"; // 0utputs: 2.5
         echo "$x % $y = ". ($x % $y) . "<br><br>"; // 0utputs: 2
 	
-    echo "Program By Bhuvanyu Chhibber (220100266)";
+    echo "Program By Bhuvanyu Chhibber (2220100266)";
 	
 
 ?>
